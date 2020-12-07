9d0b3088c6e785b9251369032db3eb4f4e747834
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-07 11:35:56 +0700

    variable in js

---
+++ novice/01-01/latihanVariabel.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 17;
+
+z = x+y;
+
+console.log(`Hasil penjumlahan x dan y adalah ${z}`);
+
+const nama = "Diko";
+
+console.log(`Nama saya adalah ${nama}`);
+
+const dataList = {nama: "Diko", umur:"22"};
+
+console.log(`Nama saya adalah ${dataList.nama} dan umur saya ${dataList.umur} tahun.`);
