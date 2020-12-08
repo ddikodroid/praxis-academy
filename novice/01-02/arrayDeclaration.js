5beef3d0bef6264d7da084bb38459500c57cb560
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-08 10:51:54 +0700

    array in js

---
+++ novice/01-02/arrayDeclaration.js
@@ -0,0 +1,13 @@
+const array1 = new Array(arrayLength=5);
+array1[0]=1
+array1[2]=2
+array1[4]=3
+
+array2 = new Array(3, 4, 5, 6, 7);
+array3 = [1, 2, 3, 4, 5]
+
+console.log(array1);
+console.log(`Banyak elemen array1 adalah ${array1.length}`);
+
+console.log(array2);
+console.log(array3);
---
+++ novice/01-02/letAndVar.js
@@ -0,0 +1,16 @@
+//Hoisting
+x = 5;
+console.log(`Nilai x adalah ${x}`); //template literal
+var x;
+//
+x = 10;
+console.log(`Nilai x apakah berubah? ${x}`);
+//
+console.log(`Nilai y adlah ${y}`);
+var y=4;
+//
+let z = 27;
+console.log("Nilai z adalah " + z);
+//
+console.log("Nilai x, y, dan z adalah " + x + ", " + y + ", " + z); 
+
