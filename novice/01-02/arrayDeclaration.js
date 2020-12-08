02850c81caf501745aec331c7b6dafe05349c408
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-08 10:59:18 +0700

    array in js

--- novice/01-02/arrayDeclaration.js
+++ novice/01-02/arrayDeclaration.js
@@ -7,7 +7,7 @@ array2 = new Array(3, 4, 5, 6, 7);
 array3 = [1, 2, 3, 4, 5]
 
 console.log(array1);
-console.log(`Banyak elemen array1 adalah ${array1.length}`);
+console.log(`Banyak elemen array1 adalah ${array1.length);
 
 console.log(array2);
 console.log(array3);
