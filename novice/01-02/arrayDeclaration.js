4e62c23604598561e604f5a017154ec9c3365b16
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-08 13:44:32 +0700

    add example for spread ...

--- novice/01-02/arrayDeclaration.js
+++ novice/01-02/arrayDeclaration.js
@@ -4,10 +4,13 @@ array1[2]=2
 array1[4]=3
 
 array2 = new Array(3, 4, 5, 6, 7);
-array3 = [1, 2, 3, 4, 5]
+array3 = [1, 2, 3, 4, 5];
 
 console.log(array1);
-console.log(`Banyak elemen array1 adalah ${array1.length});
-
+console.log(`Banyak elemen array1 adalah ${array1.length}`);
 console.log(array2);
 console.log(array3);
+
+const array4 = [...array1, ...array2];
+console.log(array4);
+console.log(`Panjang array4 adalah ${array4.length}`);
