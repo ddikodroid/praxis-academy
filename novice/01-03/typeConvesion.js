0f0e55e65852352a04a39a4aa9a7afb344ee8fd3
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-09 15:01:00 +0700

    add string conversion

---
+++ novice/01-03/typeConvesion.js
@@ -0,0 +1,6 @@
+//String Conversion
+let nilai = true;
+console.log(`Variabel nilai merupakan ${typeof nilai}`); 
+
+nilai = String(nilai); //diubah menjadi string
+console.log(`Variabel nilai merupakan ${typeof nilai}`); 
