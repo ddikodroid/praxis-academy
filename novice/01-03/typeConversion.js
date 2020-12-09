d25672688958640009099ca5c8a4767d352b10cc
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-09 15:07:30 +0700

    add numeric conversion

--- novice/01-03/typeConversion.js
+++ novice/01-03/typeConversion.js
@@ -3,4 +3,22 @@ let nilai = true;
 console.log(`Variabel nilai merupakan ${typeof nilai}`); 
 
 nilai = String(nilai); //diubah menjadi string
-console.log(`Variabel nilai merupakan ${typeof nilai}`); 
+console.log(`Variabel nilai merupakan ${typeof nilai}`);
+
+//Numeric Conversion
+console.log("28"/"4"); //division for non-number
+
+let bilangan = "274";
+console.log(`Variabel bilangan merupakan ${typeof bilangan}`);
+
+let bil = Number(bilangan);
+console.log(`Variabel bilangan merupakan ${typeof bil}`);
+
+let bukanBilangan = Number("Ini bukanlah sebuah bilangan");
+
+console.log(bukanBilangan);
+
+// undefined -> NaN
+// null -> 0
+// true and false -> 1 and 0
+console.log(Number(true));
