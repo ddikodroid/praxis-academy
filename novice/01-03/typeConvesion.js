6e08459d563c43b9f5275a8ad00098f5c797bcb2
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-09 15:07:54 +0700

    rename typo

--- novice/01-03/typeConvesion.js
+++
@@ -1,6 +0,0 @@
-//String Conversion
-let nilai = true;
-console.log(`Variabel nilai merupakan ${typeof nilai}`); 
-
-nilai = String(nilai); //diubah menjadi string
-console.log(`Variabel nilai merupakan ${typeof nilai}`); 
