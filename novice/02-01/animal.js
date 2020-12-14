92610de9994ae97c3bfad02c23580ed678ed285e
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2020-12-14 10:19:43 +0700

    module exports

---
+++ novice/02-01/animal.js
@@ -0,0 +1,19 @@
+class Animal {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    greet() {
+      console.log("Halo");
+    }
+    
+    info() {
+      this.greet();
+      console.log(`Nama saya adalah ${this.name}`);
+      console.log(`Saya berusia ${this.age} tahun`);
+    }
+  }
+
+ // module.exports = Animal
+  module.exports = {Anjing: Animal}
